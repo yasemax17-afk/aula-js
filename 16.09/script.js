61b67affa67a49952ad49f07bec17f99a68535bb
yasmin <digo.moretti3810@gmail.com>
2026-09-17 08:16:36 -0300

    conteudo aula 16.09

---
+++ 16.09/script.js
@@ -0,0 +1,138 @@
+// operadores aritimeticos
+// + soma
+// - subtração 
+// * multiplicação 
+// / divisão 
+// % sobra da divisão 
+
+// let n1 = 3; 
+// let n2 = 9;
+
+//  console .log(n1 + n2);
+//  console .log(n1-n2);
+//  console .log(n1*n2);
+//  console .log(n1/n2);
+//  console .log(n1%n2);
+//  console.log((n2-n1)*2);
+//  console.log("palavra"+ n1);
+
+//crie 3 variaveis, cada uma contendo um numero,
+// some as 3 e tire a media, exiba o resultado no console
+// let n1 = 9 
+// let n2 = 8
+// let n3 = 6
+// console.log ((n1+n2+n3) /3 );
+
+// let n1 = 6;
+// let n2 = 8;
+// let n3 = 5;
+
+// console.log("media de"+n1,n2,n3+"é"+ (n1+n2+n3) /3);
+
+// > maior que 
+// < menor que 
+// == igual
+// >= maior ou igual 
+// <= menor ou igual
+// != é diferente
+
+
+
+// let idade = 18;
+// console.log(idade>18);
+// console.log(idade<18);
+// console.log(idade=18);
+// console.log(idade>=18);
+// console.log(idade<=18);
+// console.log(idade!=18);
+
+// console.log(idade === "18"); // comparação de tipo de variaveis 
+
+// let idade = 18;
+// let habilitado = true;
+// console.log(idade <=18 && habilitado == true);
+// v && v =v
+// v && F =v
+// F && V =F
+// F && F =F
+
+// console.log(idade<18|| habilitado == true);
+// v || v =v
+// v || F =v
+// F || V =F
+// F || F =F
+
+// ! operador NOT "inverte"
+// console.log(!habilitado);
+
+
+
+//se idade for igual a 18 
+    // faça isso 
+
+//se nao 
+   // faça isso
+
+//   let habilitado = false; 
+
+//   if (habilitado == true) {// se sim
+//     console.log("pode dirigir");
+    
+//   } else {// se não
+//     console.log("NÃO pode dirigir");
+//   }
+//    let idade = 16;
+//    if (idade >=18) 
+//     if (idade >17) {
+//     console.log("maior de idade!");
+    
+//    } else {
+//     console.log("menor de idade");
+    
+//    }
+
+   //crie um sistema que compare o valor de uma variavel
+   // senha SE for igual a 1234 escreva no console 
+   // "acesso liberado" se NÃO escreva " "acesso bloqueado"
+
+//    let senha = 2409
+//    if (senha==1234) {
+//     console.log("acesso liberado");
+    
+//    } else {
+//     console.log("acesso bloqueado");
+    
+//    }
+
+ // crie um sistema que avalie uma variavel nota
+ // SE nota for maior que 6 escreva aprovado , SE NÃO 
+ // escreva reprovado
+
+//  let nota= 7
+
+//  if (nota >= 6 ) { 
+//     console.log("aprovado")
+    
+//  } else {
+//     console.log("reprovado")
+    
+//  }
+
+
+// crie um sistema que comprara um valor total de um
+// compra, SE o valor for maior que 500 calcule um
+// desconto de 20 e mostre o valor total da compra
+// com desconto SE NÃO mostre mensagem
+// COMPRA SEM DESCONTO 
+
+// let valor = 450
+// let desconto = 20
+// if (valor >= 500) {
+//     console.log(valor - 20 );
+    
+// } else {
+//     console.log("sem desconto");
+    
+// }
+
+
